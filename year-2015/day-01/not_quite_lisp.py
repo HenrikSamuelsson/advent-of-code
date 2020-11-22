351c4b70e20ffcfa85bbe0f81d51db1e8393d569
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-11-22 13:56:26 +0100

    Add solution for part 2 of 2015 day 1.

--- year-2015/day-01/not_quite_lisp.py
+++ year-2015/day-01/not_quite_lisp.py
@@ -3,10 +3,19 @@ data = file.read()
 file.close()
 
 floor = 0
+basement_found = False
+first_basement_position = 0
+current_position = 0
+
 for direction in data:
     if direction == "(":
         floor += 1
     else:
         floor -= 1
+    current_position += 1
+    if not basement_found and floor == -1:
+        first_basement_position = current_position
+        basement_found = True
 
-print(floor)
+print('Part One Answer: {}'.format(floor))
+print('Part Two Answer: {}'.format(first_basement_position))
