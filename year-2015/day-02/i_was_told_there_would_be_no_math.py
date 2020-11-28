20e7d6d31a7f7b84a0dbcd7171c7c593d1351452
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-11-28 10:04:49 +0100

    Add solution for part 2 of 2015 day 2.

--- year-2015/day-02/i_was_told_there_would_be_no_math.py
+++ year-2015/day-02/i_was_told_there_would_be_no_math.py
@@ -1,9 +1,11 @@
 import re
 
+# Get the puzzle input data that have been stored in a text file.
 file = open("input2015d02.txt")
 data = file.read()
 file.close()
 
+# Start of part one where total amount of paper is calculated.
 total_paper_area = 0
 
 for line in data.splitlines():
@@ -16,4 +18,18 @@ for line in data.splitlines():
     # Update the total paper area.
     total_paper_area += paper_area
 
-print('Part One Answer: ' + str(total_paper_area))
+# Start of part two where total amount of ribbon is calculated
+total_ribbon_length = 0
+
+for line in data.splitlines():
+    # Parse out the present dimensions in int format from the line of data.
+    dim = [int(s) for s in re.findall(r'\d+', line)]
+    # Sort the dimensions in ascending order.
+    dim.sort()
+    # Calculate how much ribbon this present needs, both for the wrapping and the bow.
+    ribbon_length = 2 * (dim[0] + dim[1]) + dim[0] * dim[1] * dim[2]
+    # Update the total ribbon length.
+    total_ribbon_length += ribbon_length
+
+print('Part one answer: ' + str(total_paper_area))
+print('Part two answer: ' + str(total_ribbon_length))
