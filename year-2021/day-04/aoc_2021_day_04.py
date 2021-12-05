b73fff99230901b8f25786c402fd684e67e38de7
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2021-12-05 16:31:01 +0100

    Update aoc_2021_day_04.py
    Solves part 1 but need to use the debugger to see the result.

--- year-2021/day-04/aoc_2021_day_04.py
+++ year-2021/day-04/aoc_2021_day_04.py
@@ -4,7 +4,7 @@ Solution to Advent of Code, year 2021, day 4.
 
 import os
 
-file = 'test-input.txt'           # Name of the file with input data.
+file = 'input.txt'           # Name of the file with input data.
 path = os.path.dirname(__file__)  # Path to the file with input data.
 file_and_path = os.path.join(path, file)
 input_file = open(file_and_path, 'r')
@@ -29,6 +29,7 @@ for i in range(2, len(lines), 6):
 
 print(bingo_boards)
 
+bingo = False
 for number in bingo_numbers:
     # Strike out number if on board
     for bingo_boards_idx, bingo_board in enumerate(bingo_boards):
@@ -37,4 +38,23 @@ for number in bingo_numbers:
                 if number == row:
                     bingo_boards[bingo_boards_idx][board_idx][row_idx] = -1
 
-print('bingo')
+    for bingo_boards_idx, bingo_board in enumerate(bingo_boards):
+        for row_idx, row in enumerate(bingo_board):
+            # Check rows for bingo
+            bingo_on_row = True
+            for row_value_idx, row_value in enumerate(row):
+                if row_value != -1:
+                    bingo_on_row = False
+                    break
+            if bingo_on_row:
+                bingo = True
+
+        # Check columns for bingo
+        for col_idx in range(5):
+            bingo_on_col = True
+            for row_idx in range(5):
+                current_num = bingo_boards[bingo_boards_idx][row_idx][col_idx]
+                if bingo_boards[bingo_boards_idx][row_idx][col_idx] != -1:
+                    bingo_on_col = False
+            if bingo_on_col:
+                bingo = True
