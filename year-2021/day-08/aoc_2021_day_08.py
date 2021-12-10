9855920321d21e8d688570f5e02fbf6bb3a4cb61
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2021-12-10 10:03:13 +0100

    Update aoc_2021_day_08.py
    Add code that solves part 1.

--- year-2021/day-08/aoc_2021_day_08.py
+++ year-2021/day-08/aoc_2021_day_08.py
@@ -2,3 +2,27 @@
 Solution to Advent of Code, year 2021, day 8.
 '''
 import os
+
+FILE = 'input.txt'                # Name of the file with input data.
+path = os.path.dirname(__file__)  # Path to the file with input data.
+file_and_path = os.path.join(path, FILE)
+input_file = open(file_and_path, 'r')
+
+lines = input_file.readlines()
+
+digit_count = [0] * 10
+
+for line in lines:
+    split_line = line.split('|')  # Split the patterns and digits apart.
+    digits = split_line[1].split()
+    for digit in digits:
+        if len(digit) == 2:
+            digit_count[1] += 1
+        elif len(digit) == 4:
+            digit_count[4] += 1
+        elif len(digit) == 3:
+            digit_count[7] += 1
+        elif len(digit) == 7:
+            digit_count[8] += 1
+
+print(sum(digit_count))
